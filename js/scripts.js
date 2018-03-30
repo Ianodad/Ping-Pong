05622b72bb076ff2f81c694cf5cf8d1dc5a59cfc
Ianodad <ianodad@gmail.com>
2018-03-30 16:55:24 +0300

    Created the user interface tho not running debugging next

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+var PingPong = function(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return "Ping Pong"
+    } else if (num % 3 === 0) {
+        return "Ping"
+    } else if (num % 5 === 0) {
+        return "Pong"
+    } else {
+        return number
+    }
+}
+
 $(document).ready(function() {
-        
-      });
+    $("form.form-number").submit(function(event) {
+        event.preventDefault();
+        var number = parsenInt($("input#digit").val());
+
+        for (var index = 0; index <= number; index++); {
+            var new_number = PingPong(index);
+            var printNumber = ("<li>" + new_number + "</li");
+            $("ul").append(printNum);
+        };
+
+        $(".result").show();
+
+
+    });
+});
