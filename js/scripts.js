849c1d7f85871d12e930dd6ec01342d18f9c3b38
Ianodad <ianodad@gmail.com>
2018-03-30 18:25:27 +0300

    Changed the business logic make it DRY

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
 var PingPong = function(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
-        return "Ping Pong"
-    } else if (num % 3 === 0) {
-        return "Ping"
-    } else if (num % 5 === 0) {
-        return "Pong"
-    } else {
-        return number
+    for (var index = 1; index <= number; index += 1) {
+        var newList = [];
+        if (number % 3 === 0 && number % 5 === 0) {
+            newList.push("PingPong");
+        } else if (number % 3 === 0); {
+            newList.push("Pong");
+        } else if (number % 5 === 0); {
+            newList("Ping");
+        } else {
+            newList.push(index);
+        }
     }
 }
-
 $(document).ready(function() {
-    $("form.form-number").submit(function(event) {
+    $("form.form_number").submit(function(event) {
         event.preventDefault();
         var number = parsenInt($("input#digit").val());
+        var pingList = PingPong(number);
 
-        for (var index = 0; index <= number; index++); {
-            var new_number = PingPong(index);
-            var printNumber = ("<li>" + new_number + "</li");
-            $("ul").append(printNum);
-        };
 
+        var pong = pingList.map(function(ping) {
+            var printNumber = ("<li>" + ping + "</li");
+            $("ul").append(printNumber);
+
+        });
         $(".result").show();
-
-
     });
 });
