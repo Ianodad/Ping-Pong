3575e1d01575604ea35d609e1b5a95352ffcca51
Ianodad <ianodad@gmail.com>
2018-04-01 18:05:17 +0300

    Complete styling of animated interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
+///////////////////////////////
+//BUSINESS LOGIC STARTS HERE!!//
+//////////////////////////////
 var PingPong = function(number) {
     for (var index = 1; index <= number; index += 1) {
         var newList = [];
         if (index % 3 === 0 && index % 5 === 0) {
-            newList.push("PingPong");
+            newList.push("Ping Pong");
         } else if (index % 3 === 0) {
             newList.push("Ping");
         } else if (index % 5 === 0) {
@@ -14,8 +17,39 @@ var PingPong = function(number) {
     return newList;
 
 };
+//js animation business logic
+// animation for Hover
+var animationHover = function(element, animation) {
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);
+        },
+        function() {
+            //wait for animation to finish before removing classes
+            window.setTimeout(function() {
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        });
+}
+// animaton for on click
+function animationClick(element, animation) {
+    element = $(element);
+    element.click(
+        function() {
+            element.addClass('animated ' + animation);
+            //wait for animation to finish before removing classes
+            window.setTimeout(function() {
+                element.removeClass('animated ' + animation);
+            }, 2000);
+
+        });
+}
 
 function countPing(numList) {}
+//////////////////////////////////////
+//USER INTERFACE LOGIC STARTS HERE!!//
+/////////////////////////////////////
 $(document).ready(function() {
     $("form.form_number").submit(function(event) {
         event.preventDefault();
@@ -30,4 +64,25 @@ $(document).ready(function() {
         });
         $(".result-tab").show();
     });
+    $(".result-tab").click(function() {
+        (".result-tab").toggle();
+
+    });
+    //Animation for hover page
+    animationHover(".heading", "tada");
+    animationHover(".btn-primary", "swing");
+    animationHover(".fall1", "hinge");
+    animationHover(".fall2", "hinge");
+    animationHover(".fall3", "hinge");
+    animationHover("#link", "shake");
+    animationHover(".btn-danger", "shake");
+    animationHover(".ping", "lightSpeedIn");
+    animationHover(".pong", "bounceInUp");
+    animationHover("#name", "hinge");
+
+    //Animation for on click
+    animationClick(".btn-primary", "hinge");
+    animationClick(".btn-danger", "zoomOut");
+    animationClick("#link", "rubberBand");
+
 });
